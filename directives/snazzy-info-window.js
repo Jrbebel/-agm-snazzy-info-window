2faea0ecc0eaceba434f6477093073c70e068797
BEBEL Jean raynal <jeanraynal.bebel@gmail.com>
2019-01-31 11:11:15 +0100

    Update snazzy-info-window.js

--- directives/snazzy-info-window.js
+++ directives/snazzy-info-window.js
@@ -127,7 +127,7 @@ var AgmSnazzyInfoWindow = (function () {
                     lng: _this.longitude
                 };
             }
-            _this._nativeSnazzyInfoWindow = new elems[0](options);
+            _this._nativeSnazzyInfoWindow = new elems[0]default.(options);
         });
         this._snazzyInfoWindowInitialized.then(function () {
             if (_this.isOpen) {
